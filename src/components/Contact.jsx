4b9a1a12b71cc106f3b5310c7057ccedb7090732
Abdulwahab Hassan <96207413+T0m0re@users.noreply.github.com>
2025-05-17 21:33:23 +0100

    Vontact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div id="conatct" className="my-20 min-h-96 w-screen px-10">
+        <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
+            
+        </div>
+    </div>
+  )
+}
+export default Contact
